a84d98a5ea7e44ea445bb4919f7047cb393c4d8d
suisun <suisun@dummy.com>
2018-04-06 17:22:07 +0800

    init

---
+++ mail_server.js
@@ -0,0 +1,28 @@
+'use strict';
+//-------------------------------------------------------//
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var fs = require("fs");
+//-------------------------------------------------------//
+// Setting environment
+app.set('views', __dirname + '/client');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(express.static(__dirname + '/client'));
+
+//-------------------------------------------------------//
+//-------------------------------------------------------//
+//-------------------------------------------------------//
+var port = process.env.PORT || 80;
+var server = app.listen(portLive, function(){
+    console.log("SocialxApp Mail Server has started on port " + portLive);
+});
