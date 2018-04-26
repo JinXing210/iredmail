6af4b06fab29afe56904b2db963330a0ea84d67a
suisun <suisun@dummy.com>
2018-04-26 17:09:01 +0800

    python

--- mail_server.js
+++ mail_server.js
@@ -28,56 +28,23 @@ var server = app.listen(port, function(){
 });
 
 require('./apis/router')(app);
-const crypto  = require('crypto');
-
-var getRandomString = function(length) {
-    return crypto.randomBytes(Math.ceil(length)/2)
-                 .toString('hex')
-                 .slice(0,length);
-}
-
-var base64 = require('js-base64').Base64
-const b64encode2  = require('base64-encode-string');
-
-var getRandomString = function(length) {
-    return crypto.randomBytes(Math.ceil(length)/2)
-                 .toString('hex')
-                 .slice(0,length);
-}
-var getSSHA512Password = function(  password ) {
-    var salt = getRandomString(8);
-    var hash = crypto.createHmac('sha512',password);
-    hash.update(salt);
-    var pass = hash.digest().toString('ascii');
-    var sshapass =  b64encode2(pass+salt);
-    return "{SSHA512}"+sshapass;
-}
-
-var sshapass = getSSHA512Password("admin")
-console.log( sshapass);
 
 
-var python = require('python-shell');
+const SMTPServer = require('smtp-server').SMTPServer;
 
-var options = {
-    args:
-    [
-        "admin"
-    ]
-}
-python.run('./apis/encodepass.py',options,function(err,data){
-    console.log( err );
-    console.log( data );
-    var pass = data[0];
-    console.log( pass );
+const mserver = new SMTPServer({
+    onMailFrom(address,session,callback) {
+        console.log( "from  " +  address.address )
+        return callback();
+    },
+    onRcptTo(address,session,callback) {
+        console.log( "rcpt  " + address.address )
+        return callback();
+    }
 })
-// console.log( base64.encode(hash2.toString('hex')+salt));
+mserver.listen(587,function(){
+    console.log("SocialxApp Mail Hook Server has started on port " + 587);
+    
+});
 
-/*
-{SSHA512}40JVd+TFOMCfR8c5SbOdBemrp7PHlqwZIFt5wjZmqxGZQcT6CtQJrASY5MTv2rc9eKqP+FPFKQ8RRIySZQ3rt6cq3Am9YJyX
-{SSHA512}VGrDLgzN/+MbSJdthAppDnM/2Dk2ZfQ4JBYDYDgDkBHZI4UqPIuvxJhcbZMS3w+HDMycbgGog/Ff9SDl+1Y3BYXMutzVZIXV
-        40JVd+TFOMCfR8c5SbOdBemrp7PHlqwZIFt5wjZmqxGZQcT6CtQJrASY5MTv2rc9eKqP+FPFKQ8RRIySZQ3rt6cq3Am9YJyX
-        BBg4DlpGSHBUfHpIUSEMA04FRnkIbhQZICI6YFJXRTAKOT0sTiQSdioXfjdeVScnHxsEAxYUa0JCJkFxfgJgEzNmMGZkZjM5
-SSZcfC5mezdROx40Hx1kSyp5Jlt4bVIaPEsDGxgmdjE4Cl8SdTc2L2FBPCcmNRI/akkwOwR1bxtlcxIcKhM9QmJlNjZkY2Ey
 
-*/
